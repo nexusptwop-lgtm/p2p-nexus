afdccdfacec426998bc2e6a6092a70c1d6ee97d7
nexusptwop-lgtm <nexusptwop@gmail.com>
2025-10-04 16:58:02 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
+
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/p2p-nexus/',
   server: {
     port: 3000,
     host: true
   },
   build: {
     target: 'es2020',
-    outDir: 'dist'
+    outDir: 'dist',
+    sourcemap: true
   },
   optimizeDeps: {
     exclude: ['ipfs-core']
